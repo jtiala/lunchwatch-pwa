2539d90d1c6c58b80f1981bcdef73632528a6f6e
Joonas Tiala <joonas.tiala@codemate.com>
2018-01-25 19:26:22 +0200

    Added a theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from 'material-ui/styles';
 import Reboot from 'material-ui/Reboot';
 import Store from './stores/Store';
-
+import Theme from './themes/Theme';
 import HeaderContainer from './containers/HeaderContainer';
 import FooterContainer from './containers/FooterContainer';
 import HomeContainer from './containers/HomeContainer';
 
-ReactDOM.render(
+const App = () => (
   <Provider store={Store}>
-    <div>
-      <Reboot />
-      <HeaderContainer />
+    <MuiThemeProvider theme={Theme}>
+      <div>
+        <Reboot />
+        <HeaderContainer />
 
-      <Router>
-        <Switch>
-          <Route component={HomeContainer} />
-        </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route component={HomeContainer} />
+          </Switch>
+        </Router>
 
-      <FooterContainer />
-    </div>
-  </Provider>,
-  document.getElementById('app'),
+        <FooterContainer />
+      </div>
+    </MuiThemeProvider>
+  </Provider>
 );
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/themes/Theme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from 'material-ui/styles';
+import orange from 'material-ui/colors/orange';
+import green from 'material-ui/colors/green';
+
+const Theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[800],
+    },
+    secondary: {
+      main: green[800],
+    },
+  },
+});
+
+export default Theme;
