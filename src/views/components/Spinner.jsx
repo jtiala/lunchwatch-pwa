4e6bc7684139b490bef7803ac5b7d07db4d9c97d
Joonas Tiala <joonas.tiala@gmail.com>
2018-02-09 22:09:48 +0200

    Add a little timer for the spinner

--- src/views/components/Spinner.jsx
+++ src/views/components/Spinner.jsx
@@ -4,6 +4,7 @@ import { compose } from 'redux';
 import { translate } from 'react-i18next';
 import { withStyles } from 'material-ui/styles';
 import { CircularProgress } from 'material-ui/Progress';
+import Fade from 'material-ui/transitions/Fade';
 
 const styles = theme => ({
   root: {
@@ -22,14 +23,52 @@ const styles = theme => ({
   },
 });
 
-const Spinner = props => (
-  <div className={props.classes.root}>
-    <div className={props.classes.wrapper}>
-      <span className={props.classes.loading} role="alert" aria-busy="true">{props.t('loading')}...</span>
-      <CircularProgress className={props.classes.progress} size={50} />
-    </div>
-  </div>
-);
+class Spinner extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.timer = null;
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+  componentWillMount() {
+    clearTimeout(this.timer);
+
+    this.timer = setTimeout(() => {
+      this.setState({
+        loading: true,
+      });
+    }, 500);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    return (
+      <div className={this.props.classes.root}>
+        <div className={this.props.classes.wrapper}>
+          <Fade
+            in={this.state.loading}
+            style={{
+              transitionDelay: this.state.loading ? '800ms' : '0ms',
+            }}
+            unmountOnExit
+          >
+            <div>
+              <span className={this.props.classes.loading} role="alert" aria-busy="true">{this.props.t('loading')}...</span>
+              <CircularProgress className={this.props.classes.progress} size={64} />
+            </div>
+          </Fade>
+        </div>
+      </div>
+    );
+  }
+}
 
 Spinner.propTypes = {
   classes: PropTypes.object.isRequired,
