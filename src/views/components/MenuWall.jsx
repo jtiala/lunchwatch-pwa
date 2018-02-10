b881f00b81fed9e141e65691719eb7099c8fa040
Joonas Tiala <joonas.tiala@gmail.com>
2018-02-10 13:39:50 +0200

    Added error message if no menus are available for the day

--- src/views/components/MenuWall.jsx
+++ src/views/components/MenuWall.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'immutable';
+import { compose } from 'redux';
+import { translate } from 'react-i18next';
 import { withStyles } from 'material-ui/styles';
 import { XMasonry, XBlock } from 'react-xmasonry/dist/index';
+import Typography from 'material-ui/Typography';
+import InfoIcon from 'material-ui-icons/Info';
 import Menu from './Menu';
 import LoadMenusButton from './LoadMenusButton';
 import Spinner from './Spinner';
 
-const styles = () => ({
+const styles = theme => ({
   root: {
     margin: '10px 10px 80px 10px',
   },
@@ -18,6 +22,25 @@ const styles = () => ({
     display: 'flex',
     justifyContent: 'center',
   },
+  noMenus: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: theme.spacing.unit * 4,
+    padding: theme.spacing.unit,
+    background: theme.palette.grey[200],
+    color: theme.palette.text.secondary,
+    borderRadius: '2px',
+    '& svg': {
+      height: 18,
+      width: 18,
+      marginRight: theme.spacing.unit,
+    },
+    '& p': {
+      flex: 1,
+      fontSize: '0.75rem',
+    },
+  },
 });
 
 const MenuWall = props => (
@@ -25,6 +48,13 @@ const MenuWall = props => (
     {props.loading && props.menus.size < 1 ?
       <div className={props.classes.center}>
         <Spinner />
+      </div> : null}
+    {!props.loading && props.menus.size < 1 ?
+      <div className={props.classes.center}>
+        <div className={props.classes.noMenus}>
+          <InfoIcon />
+          <Typography>{props.t('noMenusForSelectedDay')}</Typography>
+        </div>
       </div> :
       <div>
         <XMasonry targetBlockWidth={300}>
@@ -52,6 +82,10 @@ MenuWall.propTypes = {
   loadMore: PropTypes.func.isRequired,
   menus: PropTypes.instanceOf(List).isRequired,
   moreToLoad: PropTypes.bool.isRequired,
+  t: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles)(MenuWall);
+export default compose(
+  translate('menus'),
+  withStyles(styles),
+)(MenuWall);
