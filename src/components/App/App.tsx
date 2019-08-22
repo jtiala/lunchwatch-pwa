763e0528eb88343ca5a949924ad973366e1fe2d5
Joonas Tiala <joonas.tiala@gmail.com>
2019-08-22 16:23:27 +0300

    Add a footer component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,21 +2,37 @@ import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { AppStateProvider } from "../../appState";
 import apolloClient from "../../apolloClient";
 import theme from "../../defaultTheme";
 import Header from "../Header/Header";
 import MenuWall from "../MenuWall/MenuWall";
+import Footer from "../Footer/Footer";
+
+const useStyles = makeStyles(theme => ({
+  app: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh"
+  }
+}));
 
 const App: React.FC = () => {
+  const classes = useStyles(theme);
+
   return (
     <ApolloProvider client={apolloClient}>
       <AppStateProvider>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Header />
-          <MenuWall />
+
+          <div className={classes.app}>
+            <Header />
+            <MenuWall />
+            <Footer />
+          </div>
         </ThemeProvider>
       </AppStateProvider>
     </ApolloProvider>
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useTranslation, Trans } from "react-i18next";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import lightGreen from "@material-ui/core/colors/lightGreen";
+
+import theme from "../../defaultTheme";
+import Logo from "../Logo/Logo";
+
+const useStyles = makeStyles(theme => ({
+  footer: {
+    marginTop: "auto",
+    backgroundColor: lightGreen[50],
+    padding: theme.spacing(6),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  text: {
+    maxWidth: 600,
+    marginTop: theme.spacing(3)
+  },
+  link: {
+    color: theme.palette.text.primary,
+    "&:hover": {
+      color: theme.palette.primary.main,
+      textDecoration: "none"
+    }
+  }
+}));
+
+const Footer: React.FC = () => {
+  const classes = useStyles(theme);
+  const { i18n } = useTranslation();
+  const githubLink = (
+    <Link
+      className={classes.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://github.com/jtiala/lunchwatch-pwa/issues"
+    >
+      GitHub
+    </Link>
+  );
+
+  return (
+    <footer className={classes.footer}>
+      <Logo variant="dark" />
+      <Typography
+        variant="body2"
+        color="textSecondary"
+        align="center"
+        className={classes.text}
+      >
+        <Trans
+          i18n={i18n}
+          i18nKey="footer_info"
+          components={[githubLink]}
+        ></Trans>
+      </Typography>
+    </footer>
+  );
+};
+
+export default Footer;
