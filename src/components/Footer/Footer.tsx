9f8bd04b3abb12b88bc4fc3750f78a20f3c4a6e1
Joonas Tiala <joonas.tiala@gmail.com>
2019-10-08 13:38:02 +0300

    Remove links from footer for now

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useTranslation, Trans } from "react-i18next";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
-import { Link as RouterLink } from "react-router-dom";
 
 import useStyles from "./Footer.styles";
 import Logo from "../Logo/Logo";
@@ -10,12 +9,6 @@ import Logo from "../Logo/Logo";
 const Footer: React.FC = () => {
   const classes = useStyles();
   const { i18n, t } = useTranslation();
-
-  const privacyLink = (
-    <Link className={classes.link} component={RouterLink} to="/privacy">
-      Privacy
-    </Link>
-  );
 
   const githubLink = (
     <Link
@@ -31,9 +24,6 @@ const Footer: React.FC = () => {
   return (
     <footer className={classes.footer}>
       <Logo variant="dark" />
-      <Typography variant="body2" color="textSecondary" align="center">
-        {privacyLink} | {githubLink}
-      </Typography>
       <Typography
         variant="body2"
         color="textSecondary"
